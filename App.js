faaee4675e7e9de08032a9a0fb8e2064407d813c
Serhat Polat <SerhatLiving@hotmail.com>
2022-01-29 11:11:01 +0300

    about section added

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import {
   DefaultTheme,
   Provider as PaperProvider,
   BottomNavigation,
-  Text,
 } from "react-native-paper";
+import { View, Text, StyleSheet, Image } from "react-native";
+import profilfoto from "./assets/profilfoto.jpg";
 
 const theme = {
   ...DefaultTheme,
@@ -15,7 +16,18 @@ const theme = {
   },
 };
 
-const AboutRoute = () => <Text>About</Text>;
+const AboutRoute = () => (
+  <View style={styles.container}>
+    <Image source={profilfoto} style={styles.image} />
+    <Text style={styles.text1}>Serhat Polat</Text>
+    <Text style={styles.text2}>Frontend Developer</Text>
+    <Text style={styles.text3}>
+      I've been developing frontend for over 1 year. My main goal is to do my
+      job with the best way by constantly improving myself and to teach what I
+      know to people while doing this.
+    </Text>
+  </View>
+);
 
 const TechStackRoute = () => <Text>Tech Stack</Text>;
 
@@ -45,5 +57,39 @@ const App = () => {
     </PaperProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: "1",
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+
+  text1: {
+    color: "yellow",
+    fontWeight: "bold",
+    fontSize: 25,
+  },
+
+  text2: {
+    color: "yellow",
+  },
+
+  text3: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 20,
+  },
+
+  image: {
+    height: 150,
+    width: 150,
+    borderRadius: 75,
+    marginBottom: 10,
+  },
+});
 
 export default App;
